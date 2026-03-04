828332a34af62387bedec3645eca840cdf27212f
Evans mawira <evansmawira2004@gmail.com>
2026-03-04 10:52:33 +0300

    Added week one Assignment

---
+++ Units_Display.java
@@ -0,0 +1,12 @@
+public class Units_Display {
+    public static void main(String[] args) {
+        System.out.println("Units offered This Semester:");
+        System.out.println("1. Introduction to programming");
+        System.out.println("2. Data structures");
+        System.out.println("3. Coputer networks");
+        System.out.println("4. Database systems");
+        System.out.println("5. Software engineering");
+
+        
+    }
+}
